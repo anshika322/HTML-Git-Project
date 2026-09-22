6408969915d1b08d64257a9e9e2d09464947ffd8
anshika322 <anshik.keshri@gmail.com>
2026-09-22 13:20:12 +0530

    update student marks

--- first.py
+++ first.py
@@ -1,8 +1,8 @@
-name = "anshika"
-no1 = 60
+name = "anshi"
+no1 = 100
 no2 = 90
-no3 = 30
-no4 = 80
+no3 = 80
+no4 = 70
 total = no1 + no2 + no3 + no4
 average = total/4
 print("student name:",name)
