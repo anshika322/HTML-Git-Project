a87c3688e96abd04bf6acc0198e7e67711600bbd
anshika322 <anshik.keshri@gmail.com>
2026-09-22 11:21:16 +0530

    add student marks program

---
+++ first.py
@@ -0,0 +1,11 @@
+name = "anshika"
+no1 = 60
+no2 = 90
+no3 = 30
+no4 = 80
+total = no1 + no2 + no3 + no4
+average = total/4
+print("student name:",name)
+print("total no:",total)
+print("average marks:",average)
+
